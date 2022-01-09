bccad162a35e6be606150713cf7460960cfea816
jiangyuan <ilzyy1421@163.com>
2022-01-09 11:53:54 +0800

    增加token登录接口、token拦截器 author:jiangyuan

---
+++ springcloud-producer-order/src/main/java/com/yuan/config/WebConfigurer.java
@@ -0,0 +1,22 @@
+package com.yuan.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+    @Autowired
+    private JWTLoginInterceptor jwtLoginInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration interceptorRegistration = registry.addInterceptor(jwtLoginInterceptor);
+        interceptorRegistration.addPathPatterns("/findAll");
+        //设置不过滤的
+       /* List<String> list = new ArrayList<String>();
+        list.add("/toUserPage");
+        interceptor.excludePathPatterns(list);*/
+    }
+}
